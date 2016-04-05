9fb8ace17e9a4a6ee5b11df63216c1c2aa176f1d
Robert Chacon <chacon9595@hotmail.com>
2016-04-05 09:36:01 -0600

    creacion de header.php

---
+++ Webmail/application/views/common/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Daltic">
+
+        <title>RocsMail</title>
+        <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/css/bootstrap.min.css"); ?>" />
+
+    </head>
+<body class="index">
+
+        <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                     <?php
+                            if (is_logged()) {
+                                ?>
+                                <li>
+                                    <a href="<?php echo base_url("wa/Dashboard") ?>">Admistra Correo</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url("wa/Course/index") ?>">Administar Usuario</a>
+                                </li>
+                                <li>
+                                    <?php
+                                    echo'<a title="Cerrar Sesión" href="' . base_url("wa/Auth/logout") . '"><span class="glyphicon glyphicon-off" aria-hidden="true"></span></a>';
+                                    ?>
+                                </li>
+                                <?php } ?>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+        <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-text">
+                        <h2>Rocsmail</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
