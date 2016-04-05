5d731a988760e5d17bd0f0c7e5691ade3bbef640
Robert Chacon <chacon9595@hotmail.com>
2016-04-05 09:36:33 -0600

    preacion del archivo de scripts

---
+++ Webmail/application/views/common/scripts.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="<?php echo base_url("assets/Jquery/jquery-1.12.0.min.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/Bootstrap/js/bootstrap.min.js"); ?>"></script>
+
+</html>
