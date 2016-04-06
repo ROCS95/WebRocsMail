f6eec414c3bf2068767a4e54e98603bee3a0b882
Robert Chacon <chacon9595@hotmail.com>
2016-04-06 13:46:24 -0600

    agregue rutas de bootstrap y de kendo

--- Webmail/application/views/common/scripts.php
+++ Webmail/application/views/common/scripts.php
@@ -1,4 +1,9 @@
 <script type="text/javascript" src="<?php echo base_url("assets/Jquery/jquery-1.12.0.min.js"); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url("assets/Bootstrap/js/bootstrap.min.js"); ?>"></script>
-
+<script type="text/javascript" src="<?php echo base_url("assets/bootstrap/js/bootstrap.min.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/Kendo/js/kendo.all.min.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/Kendo/js/messages/kendo.messages.es-ES.min.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/Kendo/js/cultures/kendo.culture.es-CR.js"); ?>"></script>
+<script type="text/javascript">
+	kendo.culture("es-CR")
+</script>
 </html>
